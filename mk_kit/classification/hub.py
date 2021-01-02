0703d892315f8dae32871b94673aaeca652807b3
KumundzhievMaxim <kumundzhievmaxim@gmail.com>
2021-01-02 14:09:30 +0100

    Enforced classification ml-kit with bare models functionality.

--- mk_kit/classification/hub.py
+++ mk_kit/classification/hub.py
@@ -7,23 +7,53 @@
 # github: https://github.com/KumundzhievMaxim
 # -------------------------------------------
 
+from mk_kit.configurations import LOGGER
+
 
 class Hub:
-    def __init__(self):
-        pass
+    @staticmethod
+    def train_test_split(x, y, fraction: int, seed: int):
+        from sklearn.model_selection import train_test_split
+        LOGGER.info('Split data')
+        return train_test_split(x, y, test_size=fraction, random_state=seed)
 
-    def train_test_split(self, fraction: int):
-        pass
+    @staticmethod
+    def LogisticRegression(self, x_train, y_train, x_test, y_test, seed):
+        from sklearn.linear_model import LogisticRegression
+        from sklearn.metrics import accuracy_score
 
-    def LogisticRegression(self, x, y):
-        pass
+        # validate input shapes
+        assert len(x_train) == len(y_train), 'Unequal shapes of X_train and Y_train'
+        assert len(x_test) == len(y_test), 'Unequal shapes of X_test and Y_test'
 
-    def DecisionTree(self, x, y):
-        pass
+        LOGGER.info('Utilized Logistic Regression')
+        clf = LogisticRegression(random_state=seed).fit(x_train, y_train)
+        predictions = clf.predict(x_test)
+        score = accuracy_score(y_test, predictions)
+        LOGGER.info(f'Logistic Regression score: {score}')
+        return score, predictions
 
-    def RandomForest(self, x, y):
-        pass
+    @staticmethod
+    def DecisionTree(x_train, y_train, x_test, y_test, seed):
+        from sklearn.tree import DecisionTreeClassifier
+        from sklearn.metrics import accuracy_score
 
-    def CatBoost(self, x, y):
-        pass
+        LOGGER.info('Utilized Decision Tree Classifier')
+        clf = DecisionTreeClassifier(random_state=seed).fit(x_train, y_train)
+        predictions = clf.predict(x_test)
+        score = accuracy_score(y_test, predictions)
+        LOGGER.info(f'Decision Tree Classifier: {score}')
+        return score, predictions
+
+    @staticmethod
+    def RandomForest(x_train, y_train, x_test, y_test, seed):
+        from sklearn.ensemble import RandomForestClassifier
+        from sklearn.metrics import accuracy_score
+
+        LOGGER.info(f'Utilized Random Forest')
+        clf = RandomForestClassifier(random_state=seed).fit(x_train, y_train)
+        predictions = clf.predict(x_test)
+        score = accuracy_score(y_test, predictions)
+        LOGGER.info(f'Random Forest: {score}')
+        return score, predictions
 
